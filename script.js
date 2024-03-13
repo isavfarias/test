bfa22f1079183afbb87fef611c91d0b1aca48520
Isadora Vianna Farias <isa.viannafarias2gmail.com>
2024-03-13 15:13:53 -0300

    teste

---
+++ script.js
@@ -0,0 +1,15 @@
+// // RANDOM NUMBER  GENERATION
+// // const myBtn = document.getElementById("myBtn");
+
+// // const label1 = document.getElementById("label1");
+
+
+// // const min = 1;
+// // const max = 6;
+
+// // let randomNum1;
+
+// // myBtn.onclick = function(){
+// //     randomNum1 = Math.floor(Math.random() * max) + min;
+// //     label1.textContent = randomNum1;
+// // }
